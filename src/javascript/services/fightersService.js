439a71f18fa9eb8299a67754f84de30b10281f17
Sergey <sergey_chizhik@ukr.net>
2021-01-14 13:06:17 +0200

    Update fightersService.js

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -13,6 +13,8 @@ class FighterService {
   }
 
   async getFighterDetails(id) {
+    // todo: implement this method
+    // endpoint - `details/fighter/${id}.json`;
     try {
       const endpoint = `details/fighter/${id}.json`;
 
@@ -21,9 +23,7 @@ class FighterService {
       return apiResult;
     } catch (error) {
       throw error;
-    }
-    // todo: implement this method
-    // endpoint - `details/fighter/${id}.json`;
+    }    
   }
 }
 
